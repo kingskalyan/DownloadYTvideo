25f6c1410e1720ea504fabc9c322742327566b9b
kingskalyan <71968974+kingskalyan@users.noreply.github.com>
2021-08-14 20:35:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from pytube import YouTube
 url = "https://www.youtube.com/watch?v=VPT_EIo89cc" #write the url link of your desired video link
 video = YouTube(url) 
-print(video.title)  # it will the title of the YouTube video
+print(video.title)  # it will display the title of the YouTube video
 video = video.streams.get_highest_resolution()
+print(video.views)  # it will display views of the video
 video.download()   # Here the last step!!! to download the video
